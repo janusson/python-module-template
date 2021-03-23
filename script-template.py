f183f099f6b68b1ab3ccdf05d1c7d00dae4eb7c9
eric <34246713+janusson@users.noreply.github.com>
2021-03-23 03:20:24 -0700

    script template merge

--- script-template.py
+++ script-template.py
@@ -1,19 +1,46 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# eric janusson
+#   << module name >>
+#   Eric Janusson
+#   Python 3.9
 
 import os
 import sys
-# import module as mod
+# EX: import module as mod
 # x = mod.function_in_module()
 
+# constants
 filename = sys.argv[0]
 
+# logging
+import logging
+
+# constants
+start_time = time.time()
+# default location of Driftscope Apex3D64.exe and logfile
+apexPath = r'C:\DriftScope\lib\Apex3D64.exe'
+apexLogPath = r'C:\DriftScope\log\_Apex3DLog.txt'
+
+# logging setup # TODO
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+log_formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
+
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.INFO)
+stream_handler.setFormatter(log_formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
+
+
+# functions
 def funk():
     print(filename)
     print(os.getcwd())
     return None
 
+# main
 def main():
     funk()
 
